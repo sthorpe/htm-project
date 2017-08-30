5531c775f1a3a93c93d7c1087db54ccff0886277
Scott Thorpe <sthorpe@gmail.com>
2017-08-30 13:50:36 -0600

    Moving nav around

--- src/components/home.js
+++ src/components/home.js
@@ -10,7 +10,7 @@ export default class Home extends React.Component {
             <a href="#">Company name</a>
           </Navbar.Brand>
         </Navbar.Header>
-        <Nav>
+        <Nav pullRight>
           <NavItem eventKey={1} href="#">Product</NavItem>
           <NavItem eventKey={2} href="#">About</NavItem>
           <NavItem eventKey={3} href="#">Contact</NavItem>
