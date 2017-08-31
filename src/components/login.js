f2cb46e41b14a7d1463b136fd04a5b1d2afb4dce
Scott Thorpe <sthorpe@gmail.com>
2017-08-31 15:26:49 -0600

    Cleaned up the props a bit

--- src/components/login.js
+++ src/components/login.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { Well } from 'react-bootstrap';
 
 class Login extends React.Component {
-  onChange(e){
-   if(this.props.onChange){
-    this.props.onChange(e.target, e.target.value, e);
-   };
+  constructor(props) {
+    super(props)
+    this.state = { options: {} };
+    this.onChangeItem = this.onChangeItem.bind(this);
+    this.onSubmitItem = this.onSubmitItem.bind(this);
   }
 
-  onSubmit(e){
-   e.preventDefault();
-   if(this.props.onSubmit){
-    this.props.onSubmit(e);
-   };
+  onChangeItem(e) {
+    console.log('this seems to be working', this.props);
+    if (this.props.onChange) {
+      this.props.onChange(e.target, e.target.value, e);
+    };
+  }
+
+  onSubmitItem(e) {
+    e.preventDefault();
+    console.log(e);
   }
 
   render() {
@@ -25,30 +32,33 @@ class Login extends React.Component {
         placeholder: "Password"
       },
       checkbox: {
-        text: "Check me out"
+        text: "Remember me"
       },
       submitButton: {
         text: "Submit"
       }
    };
    options = Object.assign(options, this.props.options || {});
+
    return <div>
+      <Well>
       <form>
-        <div className="form-group">
-          <label>{options.email.label}</label>
-          <input type="email" onChange={this.onChange} className="form-control" placeholder={options.email.placeholder} />
-        </div>
-        <div className="form-group">
-          <label>{options.password.label}</label>
-          <input type="password" onChange={this.onChange}  className="form-control" placeholder={options.password.placeholder} />
-        </div>
-        <div className="checkbox">
-          <label>
-            <input type="checkbox" onChange={this.onChange}  /> {options.checkbox.text}
-          </label>
-        </div>
-        <button type="submit" onClick={this.onSubmit} className="btn btn-default">{options.submitButton.text}</button>
-      </form>
+          <div className="form-group">
+            <label>{options.email.label}</label>
+            <input type="email" onChange={this.onChangeItem} className="form-control" placeholder={options.email.placeholder} />
+          </div>
+          <div className="form-group">
+            <label>{options.password.label}</label>
+            <input type="password" onChange={this.onChangeItem}  className="form-control" placeholder={options.password.placeholder} />
+          </div>
+          <div className="checkbox">
+            <label>
+              <input type="checkbox" onChange={this.onChangeItem}  /> {options.checkbox.text}
+            </label>
+          </div>
+          <button type="submit" onClick={this.onSubmitItem} className="btn btn-default">{options.submitButton.text}</button>
+        </form>
+      </Well>
     </div>
   }
 };
